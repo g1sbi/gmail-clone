ae57849528c8a2187d249fc1e913338c466b9f8a
Luigi Panetta <luigipanetta@pm.me>
2023-02-27 12:00:50 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Header from './header.svelte';
+</script>
+
+<div>
+    <Header />
+    <a href="/inbox">Inbox</a>
+</div>
---
+++ src/routes/inbox.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let messages;
+</script>
+
+<ul>
+    {#each messages as message}
+    <li>{message.subject}</li>
+    {/each}	
+</ul>
